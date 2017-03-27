9b41c225f54cd2166f041425bcac3d35c89bb65c
Dustin Farris <dustin@farris.io>
2017-03-27 18:55:46 -0400

    feat: configure pattern for watched files
    This allows the user to specify which files trigger elm-css rebuilds.

--- index.js
+++ index.js
@@ -5,14 +5,35 @@ const path = require('path');
 
 class ElmCSSCompiler {
   constructor(config) {
+    // Get the config
     this.config = config;
     this.pluginConfig = config.plugins.elmCss || {};
+    this.ranAtLeastOnce = false;
+
+    // Ensure a sourcePath was specified by the user
     if (!this.pluginConfig.sourcePath) {
       throw "missing required config: `sourcePath`";
     }
-    this.pattern = new RegExp(this.pluginConfig.sourcePath);
+
+    // Match the main source path (e.g. Stylesheets.elm) plus all other
+    // files that could potentially affect our CSS output
+    //
+    // Default here is any .elm file, but the user can make this more
+    // specific in their brunch-config
+    this.pattern = new RegExp(
+      (new RegExp(this.pluginConfig.pattern || /\.elm/)).source + '|' +
+      (new RegExp(this.pluginConfig.sourcePath)).source
+    );
   }
   compile(file) {
+    if (file.path !== this.pluginConfig.sourcePath && !this.ranAtLeastOnce) {
+      // The initial build will match everything, but we don't want to
+      // build elm-css for n number of files, just once is fine.
+      //
+      // Going forward, rebuilds will only pass the matched file that
+      // was modified.
+      return;
+    }
     return elmCss(
       this.pluginConfig.projectDir || process.cwd(),
       this.pluginConfig.sourcePath,
@@ -26,6 +47,9 @@ class ElmCSSCompiler {
         console.log('\t- ' + result.filename);
       });
     });
+  }
+  onCompile() {
+    this.ranAtLeastOnce = true;
   }
 }
 
